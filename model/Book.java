7ae558b24a40f82d1a833dd06d789fd4df69af52
ramyasripakalapati <145890600+ramyasripakalapati@users.noreply.github.com>
2026-03-06 20:54:40 +0530

    Create Book.java

---
+++ model/Book.java
@@ -0,0 +1,30 @@
+package model;
+
+public class Book {
+
+    private int id;
+    private String title;
+    private boolean issued;
+
+    public Book(int id, String title) {
+        this.id = id;
+        this.title = title;
+        this.issued = false;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean isIssued() {
+        return issued;
+    }
+
+    public void setIssued(boolean issued) {
+        this.issued = issued;
+    }
+}
